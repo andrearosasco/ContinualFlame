57fe2cc99a41ebb0bc74cdd7756685fe1aaccda6
andrea-rosasco <andrearosasco.ar@gmail.com>
2020-10-17 17:48:47 +0200

    Cleane SplitMNIST code and added some new tests

--- cont_flame/dataset.py
+++ cont_flame/dataset.py
@@ -7,6 +7,7 @@ import numpy as np
 import requests
 import gzip
 import os
+from typing import Union
 
 
 class SplitMNIST(Dataset):
@@ -18,66 +19,54 @@ class SplitMNIST(Dataset):
             'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz']
     fnames = ['train-data', 'train-labels', 'test-data', 'test-labels']
 
-    def __init__(self, root='.', meta=None, type='train', valid=0.0, tasks=None, transform=None):
+    no_classes = 10
+    train_data, test_data = [], []
+
+    def __init__(self, root:Union[str, Path]='.', dset:str='train', valid:float=0.0, classes:list=None, transform=None):
         """
         Args:
             root (string): Directory with containing the cifar-100-python directory.
             meta (bool): True - returns the meta-training dataset, False - returns the meta-test dataset
             train (bool): True - returns the training set, False - returns the test set.
-                Training and test sets are internal to the meta-trainiing and meta-test dataset.
+                Training and test sets are internal to the meta-training and meta-test dataset.
             tasks (int): Select the tasks to keep in the dataset. If None all the tasks are used.
         """
         root = Path(root)
         self.transform = transform
 
         # download and uncompress dataset if not present
-        if not (root/'mnist-python').is_dir():
-            print('Downloading dataset...')
-            (root/'mnist-python').mkdir()
+        if len(self.train_data) == len(self.test_data) == 0:
+            if not (root/'mnist-python').is_dir():
+                self._download(root)
+            self._setup(root)
 
-            for url, fname in zip(self.urls, self.fnames):
-                r = requests.get(url)
-                fn = url.split('/')[-1]
+        if dset == 'test':
+            data = self.test_data
+        elif dset == 'train':
+            data = list(map(lambda x: x[:len(x) - int(len(x)*valid)], self.train_data))
+        elif dset == 'valid':
+            data = list(map(lambda x: x[-int(valid*len(x)):], self.train_data))
 
-                with (root/'mnist-python'/fn).open('wb') as f:
-                    f.write(r.content)
-                with gzip.open(str(root/'mnist-python'/fn), 'rb') as f:
-                    data = f.read()
-                with (root/'mnist-python'/fn[:-3]).open('wb') as f:
-                    f.write(data)
-                (root/'mnist-python'/fn).unlink()
-            print('Done!')
-
-        # open and unpickle train or test set
-        mndata = MNIST(str(root / 'mnist-python'))
-        f = mndata.load_testing() if type == 'test' else mndata.load_training()
-        imgs, labels = f
-
-        # transform dictionary in list of (x, y) pairs
-        data = np.array([np.array([np.array(d), np.array(l)]) for d, l in zip(imgs, labels)])
-
-        split = []
-        for i in range(0, 10, 2):
-            split.append(list((filter(lambda x: x[1] in [i, i + 1], data))))
-        split = np.array(split)
-
-        if type != 'test':
-            split = np.array(list(map(lambda x: x[:len(x) - int(len(x)*valid)] if type == 'train' else x[-int(valid*len(x)):], split)))
-
-        if meta is not None:
-            if meta:
-                split = split[:-2]
-            else:
-                split = split[-2:]
+        # if meta is not None:
+        #     if meta:
+        #         split = split[:-2]
+        #     else:
+        #         split = split[-2:]
 
         # select the specified tasks
-        if tasks != None and max(tasks) >= len(split):
-            print('Error: task index higher then number of tasks (#tasks=' + str(len(split) - 1) + ')')
+        if classes != None and max(classes) >= self.no_classes:
+            print('Error: Class index higher then number of classes (#classes=' + str(len(data) - 1) + ')')
         # select all the tasks (joint training)
-        if tasks == None:
-            tasks = range(len(split))
 
-        self.t = reduce(lambda x, y: np.concatenate((x, y)), split[tasks])
+        if classes == None:
+            classes = range(len(data))
+
+        t = []
+        for i in range(len(data)):
+            if i in classes:
+                t += data[i]
+
+        self.t = t
         self.l = len(self.t)
 
     def __len__(self):
@@ -96,6 +85,36 @@ class SplitMNIST(Dataset):
 
         for i in range(l):
             self.t = self.t + b
+
+    def _download(self, root):
+        print('Downloading dataset...')
+        (root/'mnist-python').mkdir(parents=True)
+
+        for url, fname in zip(self.urls, self.fnames):
+            r = requests.get(url)
+            fn = url.split('/')[-1]
+
+            with (root/'mnist-python'/fn).open('wb') as f:
+                f.write(r.content)
+            with gzip.open(str(root/'mnist-python'/fn), 'rb') as f:
+                data = f.read()
+            with (root/'mnist-python'/fn[:-3]).open('wb') as f:
+                f.write(data)
+            (root/'mnist-python'/fn).unlink()
+            print('Done!')
+
+    def _setup(self, root):
+        mndata = MNIST(str(root / 'mnist-python'))
+        train_imgs, train_labels = mndata.load_training()
+        test_imgs, test_labels = mndata.load_testing()
+
+        train_data = []
+        for i in range(self.no_classes):
+            self.train_data.append(list((filter(lambda x: x[1] == i, zip(train_imgs, train_labels)))))
+            self.test_data.append(list((filter(lambda x: x[1] == i, zip(test_imgs, test_labels)))))
+
+
+
 
 if __name__ == '__main__':
     ds = SplitMNIST(type='train', valid=0.2, tasks=[0, 1, 3])
--- tests/test_SpltiMNIST.py
+++ tests/test_SpltiMNIST.py
@@ -1,8 +1,66 @@
+import pytest
 from cont_flame.dataset import SplitMNIST
+from pathlib import Path
+from itertools import chain, combinations
+
+root = Path('./data')
+
+def test_data_dir():
+    ds = SplitMNIST(root=root)
+    assert Path(root/'mnist-python').is_dir()
 
 def test_iterate():
-    ds = SplitMNIST()
-    for x in iter(ds):
+    ds = SplitMNIST(root=root)
+    for x, y in iter(ds):
         pass
 
-test_iterate()
+def test_classes1():
+    t = range(5)
+    subsets = chain.from_iterable(combinations(t, r) for r in range(1, len(t) + 1))
+    for s in subsets:
+        ds = SplitMNIST(root=root, classes=list(s))
+        l = set([x[1] for x in iter(ds)])
+        assert set(s) == l
+
+def test_classes2():
+    examples = [5923, 6742, 5958, 6131, 5842, 5421, 5918, 6265, 5851, 5949]
+
+    t = range(5)
+    subsets = chain.from_iterable(combinations(t, r) for r in range(1, len(t) + 1))
+    for s in subsets:
+        sum = 0
+        for i in s:
+            sum += examples[i]
+
+        ds = SplitMNIST(root=root, classes=list(s))
+        assert len(ds) == sum
+
+def test_split1():
+    train = SplitMNIST(root=root, dset='train', valid=0.2, classes=[0, 1])
+    valid = SplitMNIST(root=root, dset='valid', valid=0.2, classes=[0])
+
+    for x in iter(train):
+        for y in iter(valid):
+            assert x != y
+
+
+def test_split2():
+    train = SplitMNIST(root=root, dset='train', valid=0.2, classes=[0])
+    valid = SplitMNIST(root=root, dset='valid', valid=0.3, classes=[0])
+
+    for x in iter(train):
+        for y in iter(valid):
+            if x == y:
+                assert True
+                return
+    assert False
+#
+# def test_meta():
+#     pass
+#
+# def test_transform():
+#     pass
+#
+# def test():
+#     ds = SplitMNIST(root=root, classes=[0])
+#     print(set([x[1] for x in iter(ds)]))
